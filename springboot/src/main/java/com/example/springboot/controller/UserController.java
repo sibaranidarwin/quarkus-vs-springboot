72cd0d2f9e43d3090d93d5442e0ffeb9e99c8d5e
darwinn <darwin.sibarani@ist.id>
2025-09-17 09:50:31 +0700

    Add Quarkus and springboot

---
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.User;
+import com.example.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> all() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
